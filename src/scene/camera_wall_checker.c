0e5220456099035c9dd937dea2575ec33e239553
James Lambert <lambertjamesd@gmail.com>
2025-10-14 22:19:45 -0600

    improve camera wall checking

--- src/scene/camera_wall_checker.c
+++ src/scene/camera_wall_checker.c
@@ -7,6 +7,7 @@
 #include "../collision/collision_scene.h"
 #include "../time/time.h"
 
+#define MIN_CAST_DISTANCE   0.25f
 #define MAX_CAST_DISTANCE   6.0f
 
 static struct dynamic_object_type camera_wall_checker_type = {
@@ -35,17 +36,14 @@ void camera_wall_checker_update(camera_wall_checker_t* checker, vector3_t* look_
 
     checker->position = *look_target;
     checker->cast_from = *look_target;
-    vector3Sub(position, look_target, &checker->collider.velocity);
+    struct Vector3 direction;
+    vector3Sub(position, look_target, &direction);
+    vector3Normalize(&direction, &direction);
 
-    float target_distnace = vector3MagSqrd(&checker->collider.velocity);
-
-    if (target_distnace > MAX_CAST_DISTANCE * MAX_CAST_DISTANCE) {
-        vector3Scale(&checker->collider.velocity, &checker->collider.velocity, MAX_CAST_DISTANCE / sqrt(target_distnace));
-    } else if (target_distnace < CAMERA_FOLLOW_DISTANCE * CAMERA_FOLLOW_DISTANCE) {
-        vector3Scale(&checker->collider.velocity, &checker->collider.velocity, CAMERA_FOLLOW_DISTANCE / sqrt(target_distnace));\
-    }
-
-    vector3Scale(&checker->collider.velocity, &checker->collider.velocity, 1.0f / fixed_time_step);
+    vector3AddScaled(look_target, &direction, MIN_CAST_DISTANCE, &checker->position);
+    checker->cast_from = checker->position;
+    
+    vector3Scale(&direction, &checker->collider.velocity, CAMERA_FOLLOW_DISTANCE / fixed_time_step);
 }
 
 void camera_wall_checker_destroy(camera_wall_checker_t* checker) {
