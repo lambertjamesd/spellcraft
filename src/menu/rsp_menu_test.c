eca26676b3a0180edd0cb4e05f93ba05dada6fe7
James Lambert <lambertjamesd@gmail.com>
2026-07-06 19:52:59 -0600

    cosistent rspq error replication

--- src/menu/rsp_menu_test.c
+++ src/menu/rsp_menu_test.c
@@ -25,12 +25,15 @@ void test_menu_microcode(struct test_context* t) {
     data_cache_hit_writeback(&test_mtx, sizeof(transform_2d_fp_t));
 
     rspq_write(MENU_OVERLAY_ID, MENU_CMD_SET_STACK, 10, PhysicalAddr(matrix_stack));
-    rspq_write(MENU_OVERLAY_ID, MENU_CMD_MTX, MENU_MTX_MUL | MENU_MTX_PUSH, PhysicalAddr(&test_mtx));
+    // rspq_write(MENU_OVERLAY_ID, MENU_CMD_SET_STACK, 10, PhysicalAddr(matrix_stack));
+    // rspq_write(MENU_OVERLAY_ID, MENU_CMD_MTX, MENU_MTX_MUL | MENU_MTX_PUSH, PhysicalAddr(&test_mtx));
 
     rspq_wait();
 
-    uint32_t* data = rspq_overlay_get_state(&rsp_menu);
-    debugf("%0d\n", (int)*data);
+    uint8_t* data = rspq_overlay_get_state(&rsp_menu);
+    debugf("%d\n", (int)data[0]);
+    uint16_t* mtx = (uint16_t*)(data + 32);
+    debugf("%04x %04x %04x\n%04x %04x %04x\n", (int)mtx[0], (int)mtx[1], (int)mtx[2], (int)mtx[3], (int)mtx[4], (int)mtx[5]);
     
     rspq_overlay_unregister(MENU_OVERLAY_ID);
 }
