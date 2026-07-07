a473f667bd102c1c2127627087c69f2393950d34
James Lambert <lambertjamesd@gmail.com>
2026-07-06 20:39:04 -0600

    use correct command ids

--- src/menu/rsp_menu_test.c
+++ src/menu/rsp_menu_test.c
@@ -6,12 +6,12 @@ static uint32_t MENU_OVERLAY_ID = 0;
 
 DEFINE_RSP_UCODE(rsp_menu);
 
-#define MENU_CMD_VTX        0x1
-#define MENU_CMD_LINE       0x2
-#define MENU_CMD_MTX        0x3
-#define MENU_CMD_MTX_POP    0x4
-#define MENU_CMD_SET_STACK  0x5
-#define MENU_CMD_TEST       0x6
+#define MENU_CMD_VTX        0x0
+#define MENU_CMD_LINE       0x1
+#define MENU_CMD_MTX        0x2
+#define MENU_CMD_MTX_POP    0x3
+#define MENU_CMD_SET_STACK  0x4
+#define MENU_CMD_TEST       0x5
 
 static transform_2d_fp_t matrix_stack[10];
 
@@ -25,8 +25,7 @@ void test_menu_microcode(struct test_context* t) {
     data_cache_hit_writeback(&test_mtx, sizeof(transform_2d_fp_t));
 
     rspq_write(MENU_OVERLAY_ID, MENU_CMD_SET_STACK, 10, PhysicalAddr(matrix_stack));
-    // rspq_write(MENU_OVERLAY_ID, MENU_CMD_SET_STACK, 10, PhysicalAddr(matrix_stack));
-    // rspq_write(MENU_OVERLAY_ID, MENU_CMD_MTX, MENU_MTX_MUL | MENU_MTX_PUSH, PhysicalAddr(&test_mtx));
+    rspq_write(MENU_OVERLAY_ID, MENU_CMD_MTX, MENU_MTX_MUL | MENU_MTX_PUSH, PhysicalAddr(&test_mtx));
 
     rspq_wait();
 
